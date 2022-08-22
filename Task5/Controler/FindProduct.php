8e8a1f81fc548f7e77f5a750673e3577f9c1649f
fazidan <106707204+fazidan@users.noreply.github.com>
2022-08-22 12:31:40 +0600

    Add files via upload

---
+++ Task5/Controler/FindProduct.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once '../Model/Model.php';
+
+if (isset($_POST['find'])) {
+	
+		echo $_POST['id'];
+
+    try {
+    	
+    	$allSearchedUsers = searchProduct($_POST['id']);
+    	require_once '../showSearchedProduct.php';
+
+    } catch (Exception $ex) {
+    	echo $ex->getMessage();
+    }
+}
